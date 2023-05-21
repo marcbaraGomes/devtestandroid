226b6e4f86335cd3a545ec19ced339ffed9e55ab
marcbaraGomes <49415332+marcbaraGomes@users.noreply.github.com>
2023-05-21 01:04:39 -0300

    Primeira versão

---
+++ app/src/main/java/com/marcbaragom/testeslider/MainActivity.java
@@ -0,0 +1,16 @@
+package com.marcbaragom.testeslider;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+
+public class MainActivity extends IntroActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
